e8e46ea811a2fb3ad0348a27f5ecb35c81dbdf7d
somecirrusmoney <jlevalley@salesforce.com>
2015-06-23 13:56:09 -0700

    Initial Starting Point
    App timer scaffolding

---
+++ processtimer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+import psycopg2
+import sys
+import xml.etree.ElementTree as ET
+import urllib3
+import codecs
+from datetime import datetime, date, time
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=10)
+def scheduled_job():
+    print('This job is run every weekday at 5pm.')
+
+sched.start()
